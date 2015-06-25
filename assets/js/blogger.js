15e97e18f961997849459c70a3aa057bfcc6bfb4
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-06-25 09:12:33 -0500

    Test Push

---
+++ assets/js/blogger.js
@@ -0,0 +1,53 @@
+function getblog() {
+// Blog entry should be diplayed with the following format:
+// <div id='key' class='col-sm-12 blogpost'> 
+//   <small>
+//     <p class='muted' style='float:right;'>Post date will go here.</p>
+//   </small>
+//   <h5>Post title will go here.</h5>
+//   <p>Post content will go here.</p>
+//   <hr/>
+// </div>
+
+	entryHTML = "";
+
+	// Add Code to read the entries...
+
+
+$.getJSON("entries.json", function(json) {
+    console.log(json);
+	});
+
+	// entries = ge
+
+	// for (i in entries) {
+	// 	entry = entries[i];
+	// 	entryHTML += convertEntryToHTML(entry);
+	// };
+	// document.getElementById("blog").innerHTML = entryHTML;
+};
+
+
+
+
+
+
+function convertEntryToHTML (entry) {
+	//console.log(entry);
+	textEntry = 
+		"<small>" +
+			"<p class='muted' style='float:right;'>" + entry["date"] + "</p>" +
+  		"</small>" +
+  		"<h4>" + entry["title"] +"</h4>" +
+  		"<p>" + entry["text"] + "</p>" +
+  		"<hr/>";
+
+  	return textEntry;
+};
+
+
+
+
+
+
+$(document).ready(getblog);
